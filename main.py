63c1db7d0ebac7291829d97a9041d697e597b472
a.pivkin <a.pivkin@norsi-trans.ru>
2022-10-27 15:17:43 +0300

    Added VPN client '{}'

--- main.py
+++ main.py
@@ -3,65 +3,86 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from fileinput import close
-from socket import socket
 import subprocess
 import paramiko
 import sys
-import os
 import configparser
+import os
+from pathlib import Path
 
-
-
-_user = "user"
+_user = "root"
 _port = 3578
-_password = "qweqwe"
+hostname_short = "combined-ipdr01"
 hostname = ""
+VPN_ip = ""
+_5octets = ""
+VPN_git = ""
 
 
-def config():
+def config_write():
     config_file = configparser.ConfigParser()
     config_file['DEFAULT'] = {}
-    config_file['DEFAULT']['PUPPET_GIT_FOLDER'] = input("Enter location of Puppet git folder: ")
+    config_file['DEFAULT']['VPN_GIT_FOLDER'] = input("Enter location of VPN git folder: ")
     with open('config.ini', 'w') as f:
         config_file.write(f)
+
+
+def config_read():
+    global VPN_git
+    config_file = configparser.ConfigParser()
+    config_file.read('config.ini')
+    VPN_git = config_file['DEFAULT']['VPN_GIT_FOLDER']
+    print(VPN_git)
+
 
 
 def ssh_connect():
     try:
         client = paramiko.client.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(sys.argv[1], port=_port, username=_user, password=_password)
-        _stdin, _stdout, _stderr = client.exec_command('cd /home/user; echo {}'.format(hostname) + '> test_file; hostname -f')
+        client.connect(sys.argv[1], port=_port, username=_user)
+        _stdin, _stdout, _stderr = client.exec_command(
+            'sed -i 2d /etc/hosts; sed -i \'2i{}\''.format(hostname) + ' /etc/hosts;' + 'echo {}'.format(hostname_short) + ' > /etc/hostname; '
+                                                                                                                           'hostname -F /etc/hostname;'
+                                                                                                                           'hostname -f')
+        print("\n----------------")
         print(_stdout.read().decode())
-        print("I am here")
     except TimeoutError:
         print("\n{}Host doesnt respond{}".format('\033[1m', '\033[0m'))
         sys.exit()
     finally:
-        client.close()                                                                                          
+        client.close()
 
 
-def set_hostname(host_name):
+def set_credentials(segment, provider_name, city, ip):
     global hostname
-    hostname = "ubuntu.home.{}".format(host_name)
+    hostname = "127.0.1.1       combined-ipdr01.sorm-2-3.{}.{}.{}.prod.s8.norsi-trans.org   combined-ipdr01".format(segment, provider_name, city)
+    global _5octets
+    _5octets = hostname.removesuffix(".prod.s8.norsi-trans.org   combined-ipdr01").removeprefix("127.0.1.1       ")
+    print("\n5 octets are {}".format(_5octets))
+    global VPN_ip
+    VPN_ip = ip
 
 
 if __name__ == '__main__':
     if not os.path.isfile('config.ini'):
         choice = input("Do you want to create default settings config? (y/n) ")
         if choice == "y":
-            config()
+            config_write()
         else:
             pass
-
-    set_hostname(sys.argv[2])
+    config_read()
     print("---------------------\n" + "Script name is: ", sys.argv[0] + "\n---------------------")
     print("IP address is: ", sys.argv[1] + "\n---------------------")
-    print("Hostname is: ", sys.argv[2] + "\n---------------------")
+    print("Segment is: ", sys.argv[2] + "\n---------------------")
+    print("Provider name is: ", sys.argv[3] + "\n---------------------")
+    print("City is: ", sys.argv[4] + "\n---------------------")
+    print("IP address is (the last octet): ", sys.argv[5] + "\n---------------------")
+
+    set_credentials(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
     ssh_connect()
-    print("I am here 1")
-    ans = input("Is the FQDN correct? (y/n): ") # Эта хуйня в винде почему то не работает
+
+    ans = input("\nIs the FQDN correct? (y/n): ")  # Эта хуйня в винде почему то не работает
     match ans:
         case "y":
             print("OK")
@@ -72,11 +93,29 @@ if __name__ == '__main__':
         case _:
             print("Wrong answer, motherfucker!")
             sys.exit()
-            
-    print("PROCEEDING")
-    cmd = "ssh {}@{} -p{}".format(_user, sys.argv[1], _port)
-    input_cmd = "cd /home/user/TEST; echo 'HUI' >> test"
-    res = subprocess.run(cmd, stdout=subprocess.PIPE, input=input_cmd, check=True
-                         , shell=True
-                         , text=True)
 
+    print("\n========================\nPROCEEDING\n ============================")
+
+    try:
+        VPN_CMD = "cd {}/easy-rsa; ./easyrsa gen-req {} nopass; ./easyrsa sign-req client {}".format(VPN_git, _5octets, _5octets)
+        subprocess.run(VPN_CMD, check=True, shell=True)
+    except subprocess.CalledProcessError:
+        print("Some shit happened :\'(")
+
+    try:
+        VPN_CMD = "cd {}/ccd; touch {}; echo 'ifconfig-push 172.24.112.{} 255.255.240.0' > {}".format(VPN_git, _5octets, VPN_ip, _5octets)
+        subprocess.run(VPN_CMD, check=True, shell=True)
+    except subprocess.CalledProcessError:
+        print("Some shit happened :\'(")
+
+    try:
+        VPN_CMD = "cd {}/scripts; ./create_client_ovpn.sh {}".format(VPN_git, _5octets)
+        subprocess.run(VPN_CMD, check=True, shell=True)
+    except subprocess.CalledProcessError:
+        print("Some shit happened :\'(")
+
+    try:
+        VPN_CMD = "cd {}; git add --all; git commit -m \"Added VPN client '{}'\"; git push"
+        subprocess.run(VPN_CMD, check=True, shell=True)
+    except subprocess.CalledProcessError:
+        print("Some shit happened :\'(")
