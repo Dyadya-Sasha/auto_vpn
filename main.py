1366fecd75de4e62e5462d6dbffa04187ebf40d4
a.pivkin <a.pivkin@norsi-trans.ru>
2022-10-31 13:58:42 +0300

    Added iptables for client 'svyazinvestregion'

--- main.py
+++ main.py
@@ -9,25 +9,36 @@ _user = "root"
 _port = 3578
 hostname_short = "combined-ipdr01"
 hostname = ""
+hosts_line = ""
 VPN_ip = ""
 _5octets = ""
 VPN_git = ""
+PUPPET_GIT = ""
+DOMAIN_NAME = ""
+puppet_repo = "172.24.112.1    master01.main01.puppet.infra.prod.int.local.norsi-trans.org\n"
 
 
 def config_write():
     config_file = configparser.ConfigParser()
     config_file['DEFAULT'] = {}
     config_file['DEFAULT']['VPN_GIT_FOLDER'] = input("Enter location of VPN git folder: ")
+    config_file['DEFAULT']['PUPPET_GIT_FOLDER'] = input("Enter location of Puppet git folder: ")
+    config_file['DEFAULT']['DOMAIN_NAME'] = input("Enter your domain name: ")
     with open('config.ini', 'w') as f:
         config_file.write(f)
 
 
 def config_read():
     global VPN_git
+    global PUPPET_GIT
+    global DOMAIN_NAME
     config_file = configparser.ConfigParser()
     config_file.read('config.ini')
     VPN_git = config_file['DEFAULT']['VPN_GIT_FOLDER']
+    PUPPET_GIT = config_file['DEFAULT']['PUPPET_GIT_FOLDER']
+    DOMAIN_NAME = config_file['DEFAULT']['DOMAIN_NAME']
     print(VPN_git)
+    print(PUPPET_GIT)
 
 
 def ssh_connect():
@@ -36,9 +47,10 @@ def ssh_connect():
         client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client1.connect(sys.argv[1], port=_port, username=_user)
         _stdin1, _stdout1, _stderr1 = client1.exec_command(
-            'sed -i 2d /etc/hosts; sed -i \'2i{}\''.format(hostname) + ' /etc/hosts;' + 'echo {}'.format(hostname_short) + ' > /etc/hostname; '
-                                                                                                                           'hostname -F /etc/hostname;'
-                                                                                                                           'hostname -f')
+            'sed -i 2d /etc/hosts; sed -i \'2i127.0.1.1     {}  combined-ipdr01\''.format(hostname) + ' /etc/hosts;' + 'sed -i 3d /etc/hosts; sed -i \'3i{}\' /etc/hosts;'.format(puppet_repo) + 'echo {}'.format(hostname_short) + ' > /etc/hostname; '
+                                                                                                                                                                               'hostname -F '
+                                                                                                                                                                               '/etc/hostname; '
+                                                                                                                                                                               'hostname -f')
         print("\n----------------")
         print(_stdout1.read().decode())
     except TimeoutError:
@@ -50,17 +62,26 @@ def ssh_connect():
 
 def set_credentials(segment, provider_name, city, ip):
     global hostname
-    hostname = "127.0.1.1       combined-ipdr01.sorm-2-3.{}.{}.{}.prod.s8.norsi-trans.org   combined-ipdr01".format(segment, provider_name, city)
+    global hosts_line
+    hostname = "combined-ipdr01.sorm-2-3.{}.{}.{}.prod.s8.norsi-trans.org".format(segment, provider_name, city)
+    hosts_line = "127.0.1.1       {}   combined-ipdr01".format(hostname)
     global _5octets
-    _5octets = "combined-ipdr01.sorm-2-3.{}.{}.{}".format(segment,provider_name, city)
+    _5octets = "combined-ipdr01.sorm-2-3.{}.{}.{}".format(segment, provider_name, city)
     print("\n5 octets are {}\n".format(_5octets))
     global VPN_ip
     VPN_ip = ip
 
 
+def puppet_prepare(segment, provider_name, city):
+    puppet_cmd = "mkdir -p {}/s8/prod/{}/{}/{}/sorm-2-3/combined-ipdr/nodes;".format(PUPPET_GIT, city, provider_name, segment)
+    subprocess.run(puppet_cmd, check=True, shell=True)
+    puppet_cmd = "cat /home/$USER/puppet_skel > {}/s8/prod/{}/{}/{}/sorm-2-3/combined-ipdr/nodes/combined-ipdr01.sorm-2-3.{}.{}.{}.prod.s8.norsi-trans.org.yaml".format(PUPPET_GIT, city, provider_name, segment, segment, provider_name, city)
+    subprocess.run(puppet_cmd, check=True, shell=True)
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        sys.exit("Not enough parameters")
+    #    if len(sys.argv) < 7:
+    #        sys.exit("Not enough parameters")
 
     if not os.path.isfile('config.ini'):
         choice = input("Do you want to create default settings config? (y/n) ")
@@ -82,6 +103,7 @@ if __name__ == '__main__':
     print("IP address is (the last octet): ", sys.argv[5] + "\n---------------------")
 
     set_credentials(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    puppet_prepare(sys.argv[2], sys.argv[3], sys.argv[4])
     ssh_connect()
 
     ans = input("\nIs the FQDN correct? (y/n): ")  # Эта хуйня в винде почему то не работает
@@ -114,20 +136,60 @@ if __name__ == '__main__':
         VPN_CMD = "cd {}/keys; scp -P{} sorm01-prod-dmz_{}.ovpn {}@{}:/root".format(VPN_git, _port, _5octets, _user, sys.argv[1])
         subprocess.run(VPN_CMD, check=True, shell=True)
 
-        val = input("\n\nDO NOT FORGET TO RUN 'sudo /opt/puppetlabs/bin/puppet agent -tv' on VPN server ( <domain_name>@10.61.5.11 ), \
-        OTHERWISE U WILL GET FAILED WITH VPN AUTH. RUN IT AND PRESS ANY KEY TO CONFIRM AFTER PUPPET AGENT IS DONE: ")
+        print("\n\nRUNNING PUPPET AGENT ON VPNGW")
 
-        print("Installing OpenVPN on remote host")
-        #        ssh_Process = subprocess.run(['ssh {}'.format(VPN_CMD)], stdout=subprocess.PIPE, universal_newlines=True, shell=True, check=True)
+        vpn_ssh = client = paramiko.client.SSHClient()
+        vpn_ssh .set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        vpn_ssh.connect('10.61.5.11', username=DOMAIN_NAME)
+        _stdout_, _stdin_, _stderr_ = vpn_ssh.exec_command('sudo /opt/puppetlabs/bin/puppet agent -tv')
+        exit_status = _stdout_.channel.recv_exit_status()
+        print(exit_status)
+        if exit_status == 2:
+            print("Puppet agent on VPN did its work successfully")
+        else:
+            print("Puppet agent on VPN failed")
+        vpn_ssh.close()
+
+        val = input("\n\nDO NOT FORGET TO CORRECT IPTABLES RULES BEFORE CONTINUING. PRESS ENTER TO CONFIRM.")
+        Puppet_push = ("cd /home/$USER/{}; git add --all; git commit -m \"Added iptables for client '{}'\"; git push ".format(PUPPET_GIT, sys.argv[3]))
+        print("\nPUSHING Pupper CMD {}".format(Puppet_push))
+        subprocess.run(Puppet_push, check=True, shell=True)
+
+        print("\nInstalling OpenVPN and puppet agent on remote host")
         client = paramiko.client.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(sys.argv[1], port=_port, username=_user)
-        _stdin, _stdout, _stderr = client.exec_command('ssh {}@{} -p{}'.format(_user, sys.argv[1], _port) + '; cd /etc/apt/sources.list.d/; rm nt.list*; apt update; apt -y install openvpn; ' +
+        _stdin, _stdout, _stderr = client.exec_command(' cd /etc/apt/sources.list.d/; rm nt.list*; apt -y install openvpn;' +
                                                        'mv /root/sorm01-prod-dmz_{}.ovpn /etc/openvpn/sorm01-prod-dmz_{}.conf; '.format(_5octets, _5octets) +
                                                        'systemctl enable openvpn@sorm01-prod-dmz_{}; '.format(_5octets) +
-                                                       'systemctl start openvpn@sorm01-prod-dmz_{}'.format(_5octets))
-        _stdout.channel.set_combine_stderr(True)
-        output = _stdout.readlines()
+                                                       'systemctl start openvpn@sorm01-prod-dmz_{};'.format(_5octets) + 'iptables-save > /etc/iptables.bkp;' + 'echo deb http://172.24.112.1/apt'
+                                                                                                                                                               '.puppetlabs.com bionic puppet7 > '
+                                                                                                                                                               '/etc/apt/sources.list.d/puppet7.list; '
+                                                                                                                                                               'apt update;' +
+                                                       'wget http://172.24.112.1/grcc/apt/puppet5.asc; apt-key add puppet5.asc; apt -y install puppet-agent;' +
+                                                       'sudo /opt/puppetlabs/bin/puppet agent -tv --server master01.main01.puppet.infra.prod.int.local.norsi-trans.org;')
+        exit_status = _stdout.channel.recv_exit_status()
+        print("Sign cert request\n")
+        print(exit_status)
+        if exit_status == 0:
+            print("\nCOMPLETED")
+        else:
+            print("\nSomething went wrong")
+        client.close()
+
+        puppet_ssh = client = paramiko.client.SSHClient()
+        puppet_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        puppet_ssh.connect('10.60.5.11', username=DOMAIN_NAME)
+        _stdout_, _stdin_, _stderr_ = puppet_ssh.exec_command('sudo /opt/puppetlabs/bin/puppetserver ca sign —-certname={}'.format(hostname))
+        exit_status = _stdout_.channel.recv_exit_status()
+
+        print("\npuppet git server exit status\n")
+        print(exit_status)
+        if exit_status == 2:
+            print("Puppet agent on Puppet_git did its work successfully")
+        else:
+            print("Puppet agent on Puppet_git failed")
+        puppet_ssh.close()
 
     except subprocess.CalledProcessError:
         print("Some shit happened :\'(")
