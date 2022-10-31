e41d4c3272cc0cf19ae03bad549937a76c81574b
a.pivkin <a.pivkin@norsi-trans.ru>
2022-10-31 18:08:43 +0300

    The last one for 27.10

--- main.py
+++ main.py
@@ -47,10 +47,11 @@ def ssh_connect():
         client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client1.connect(sys.argv[1], port=_port, username=_user)
         _stdin1, _stdout1, _stderr1 = client1.exec_command(
-            'sed -i 2d /etc/hosts; sed -i \'2i127.0.1.1     {}  combined-ipdr01\''.format(hostname) + ' /etc/hosts;' + 'sed -i 3d /etc/hosts; sed -i \'3i{}\' /etc/hosts;'.format(puppet_repo) + 'echo {}'.format(hostname_short) + ' > /etc/hostname; '
-                                                                                                                                                                               'hostname -F '
-                                                                                                                                                                               '/etc/hostname; '
-                                                                                                                                                                               'hostname -f')
+            'sed -i 2d /etc/hosts; sed -i \'2i127.0.1.1     {}  combined-ipdr01\''.format(hostname) + ' /etc/hosts;' + 'sed -i 3d /etc/hosts; sed -i \'3i{}\' /etc/hosts;'.format(
+                puppet_repo) + 'echo {}'.format(hostname_short) + ' > /etc/hostname; '
+                                                                  'hostname -F '
+                                                                  '/etc/hostname; '
+                                                                  'hostname -f')
         print("\n----------------")
         print(_stdout1.read().decode())
     except TimeoutError:
@@ -75,7 +76,9 @@ def set_credentials(segment, provider_name, city, ip):
 def puppet_prepare(segment, provider_name, city):
     puppet_cmd = "mkdir -p {}/s8/prod/{}/{}/{}/sorm-2-3/combined-ipdr/nodes;".format(PUPPET_GIT, city, provider_name, segment)
     subprocess.run(puppet_cmd, check=True, shell=True)
-    puppet_cmd = "cat /home/$USER/puppet_skel > {}/s8/prod/{}/{}/{}/sorm-2-3/combined-ipdr/nodes/combined-ipdr01.sorm-2-3.{}.{}.{}.prod.s8.norsi-trans.org.yaml".format(PUPPET_GIT, city, provider_name, segment, segment, provider_name, city)
+    puppet_cmd = "cat /home/$USER/puppet_skel > {}/s8/prod/{}/{}/{}/sorm-2-3/combined-ipdr/nodes/combined-ipdr01.sorm-2-3.{}.{}.{}.prod.s8.norsi-trans.org.yaml".format(PUPPET_GIT, city, provider_name,
+                                                                                                                                                                        segment, segment, provider_name,
+                                                                                                                                                                        city)
     subprocess.run(puppet_cmd, check=True, shell=True)
 
 
@@ -139,7 +142,7 @@ if __name__ == '__main__':
         print("\n\nRUNNING PUPPET AGENT ON VPNGW")
 
         vpn_ssh = client = paramiko.client.SSHClient()
-        vpn_ssh .set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        vpn_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         vpn_ssh.connect('10.61.5.11', username=DOMAIN_NAME)
         _stdout_, _stdin_, _stderr_ = vpn_ssh.exec_command('sudo /opt/puppetlabs/bin/puppet agent -tv')
         exit_status = _stdout_.channel.recv_exit_status()
@@ -151,8 +154,8 @@ if __name__ == '__main__':
         vpn_ssh.close()
 
         val = input("\n\nDO NOT FORGET TO CORRECT IPTABLES RULES BEFORE CONTINUING. PRESS ENTER TO CONFIRM.")
-        Puppet_push = ("cd /home/$USER/{}; git add --all; git commit -m \"Added iptables for client '{}'\"; git push ".format(PUPPET_GIT, sys.argv[3]))
-        print("\nPUSHING Pupper CMD {}".format(Puppet_push))
+        Puppet_push = ("cd {}; git add --all; git commit -m \"Added iptables for client '{}'\"; git push ".format(PUPPET_GIT, sys.argv[3]))
+        print("\nPUSHING Puppet CMD {}".format(Puppet_push))
         subprocess.run(Puppet_push, check=True, shell=True)
 
         print("\nInstalling OpenVPN and puppet agent on remote host")
@@ -167,11 +170,11 @@ if __name__ == '__main__':
                                                                                                                                                                '/etc/apt/sources.list.d/puppet7.list; '
                                                                                                                                                                'apt update;' +
                                                        'wget http://172.24.112.1/grcc/apt/puppet5.asc; apt-key add puppet5.asc; apt -y install puppet-agent;' +
-                                                       'sudo /opt/puppetlabs/bin/puppet agent -tv --server master01.main01.puppet.infra.prod.int.local.norsi-trans.org;')
+                                                       'sudo /opt/puppetlabs/bin/puppet agent -tv --server master01.main01.puppet.infra.prod.int.local.norsi-trans.org')
         exit_status = _stdout.channel.recv_exit_status()
-        print("Sign cert request\n")
+        print("\nSign cert request\n")
         print(exit_status)
-        if exit_status == 0:
+        if exit_status == 2:
             print("\nCOMPLETED")
         else:
             print("\nSomething went wrong")
@@ -180,16 +183,28 @@ if __name__ == '__main__':
         puppet_ssh = client = paramiko.client.SSHClient()
         puppet_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         puppet_ssh.connect('10.60.5.11', username=DOMAIN_NAME)
-        _stdout_, _stdin_, _stderr_ = puppet_ssh.exec_command('sudo /opt/puppetlabs/bin/puppetserver ca sign —-certname={}'.format(hostname))
+        _stdout_, _stdin_, _stderr_ = puppet_ssh.exec_command('sudo /opt/puppetlabs/bin/puppetserver ca sign --certname={}'.format(hostname))
         exit_status = _stdout_.channel.recv_exit_status()
 
         print("\npuppet git server exit status\n")
         print(exit_status)
-        if exit_status == 2:
-            print("Puppet agent on Puppet_git did its work successfully")
+        if exit_status == 1:
+            print("Certificate onPuppet_git is signed successfully")
         else:
             print("Puppet agent on Puppet_git failed")
         puppet_ssh.close()
+
+    #        final_exit = 1
+    #        while final_exit != 0:
+    #           client1 = paramiko.client.SSHClient()
+    #           client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #           client1.connect(sys.argv[1], port=_port, username=DOMAIN_NAME)
+    #           _stdin1, _stdout1, _stderr1 = client1.exec_command('sudo /opt/puppetlabs/bin/puppet agent -tv --server master01.main01.puppet.infra.prod.int.local.norsi-trans.org')
+    #           print("\n{}".format(final_exit))
+    #           print("one mode final step")
+    #           final_exit = _stdout1.channel.recv_exit_status()
+    #           if final_exit == 0:
+    #                print("Done!")
 
     except subprocess.CalledProcessError:
         print("Some shit happened :\'(")
